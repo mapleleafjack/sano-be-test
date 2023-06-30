9aba19eadf89e4502f839d45168b182ea693d02f
mapleleafjack <jack.musajo@madetech.com>
2023-06-30 23:59:40 +0100

    Added UI

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from core.blueprints import register_blueprints
 from core.models import create_tables
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static', static_folder='static')
     register_blueprints(app)
 
     @app.route("/")
     def hello_world():
-        return "Hello, World!"
+        return render_template('index.html')
 
     return app
 
