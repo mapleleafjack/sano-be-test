2d0372992f4c43c236a5bfe366a90ad5b88b0697
mapleleafjack <jack.musajo@madetech.com>
2023-07-02 22:41:37 +0100

    Focus on: test for task 3 - part 2

--- tests/blueprints/test_users.py
+++ tests/blueprints/test_users.py
@@ -2,6 +2,7 @@ from unittest import mock
 import uuid
 from core.models import User
 from core.use_cases.add_user_usecase import AddUserUsecase
+from core.use_cases.get_all_users_usecase import GetAllUsersUsecase
 
 
 class TestUsers:
@@ -73,3 +74,47 @@ class TestUsers:
 
         assert response.status_code == 200
         assert response.json == 1
+
+    @mock.patch.object(GetAllUsersUsecase, "__call__")
+    def test_get_all_users_api_calls_usecase(self, usecase, test_client):
+        usecase.return_value = []
+
+        response = test_client.get("/users")
+
+        assert response.status_code == 200
+        usecase.assert_called_once()
+
+    @mock.patch.object(GetAllUsersUsecase, "__call__")
+    def test_get_all_users_api_returns_list_of_users_with_order_information(
+        self, usecase, test_client
+    ):
+        user_id = uuid.uuid4()
+        usecase.return_value = [
+            {
+                "email": "test.testington@gmail.com",
+                "phone_number": "123456789",
+                "id": user_id,
+                "name": "Test Testington",
+                "created_at": "2023-07-02T22:25:42.526744",
+                "orders": [
+                    {
+                        "created_at": "2023-07-02T22:26:37.481275",
+                        "user": {
+                            "phone_number": "07858512504",
+                            "created_at": "2023-07-02T22:25:42.526744",
+                            "email": "jack.musajo@gmail.com",
+                            "name": "Chakri Musajo Somma",
+                            "id": user_id,
+                        },
+                        "sequencing_type": "whole-exome-sequencing",
+                        "shipping_info": {"some": "info"},
+                        "id": "KLNAuaEtUvGaJTgfCRU9ro",
+                    }
+                ],
+            }
+        ]
+        response = test_client.get("/users")
+
+        assert response.status_code == 200
+        assert "orders" in response.json["data"][0]
+        assert (len(response.json["data"][0]["orders"])) == 1
